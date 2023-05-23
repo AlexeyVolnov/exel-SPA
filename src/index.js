2a19417a126468979cc5e57fe5b159865b543946
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-05-23 15:17:10 +0500

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import _ from 'lodash'
-import './scss/style.scss'
-
-// eslint-disable-next-line require-jsdoc
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import './scss/style.scss';
 
 
